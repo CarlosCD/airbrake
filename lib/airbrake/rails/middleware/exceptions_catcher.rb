a8abd4b73ffc5d397398e5111740b8441d72c082
Carlos A. Carro Duplá <cdupla@stsci.edu>
2016-11-01 16:53:04 -0400

    Removing calls to alias_method_chain (Rails 5)

--- lib/airbrake/rails/middleware/exceptions_catcher.rb
+++ lib/airbrake/rails/middleware/exceptions_catcher.rb
@@ -1,27 +1,30 @@
+# encoding: utf-8
+
 module Airbrake
   module Rails
     module Middleware
       module ExceptionsCatcher
         def self.included(base)
-          base.send(:alias_method_chain,:render_exception,:airbrake)
+          base.send(:alias_method, :render_exception_without_airbrake, :render_exception)
+          base.send(:alias_method, :render_exception, :render_exception_with_airbrake)
         end
 
         def skip_user_agent?(env)
-          user_agent = env["HTTP_USER_AGENT"]
+          user_agent = env['HTTP_USER_AGENT']
           ::Airbrake.configuration.ignore_user_agent.flatten.any? { |ua| ua === user_agent }
         rescue
           false
         end
 
-        def render_exception_with_airbrake(env,exception)
+        def render_exception_with_airbrake(env, exception)
           controller = env['action_controller.instance']
           env['airbrake.error_id'] = Airbrake.
             notify_or_ignore(exception,
-                   controller.airbrake_request_data) unless skip_user_agent?(env)
+                             controller.airbrake_request_data) unless skip_user_agent?(env)
           if defined?(controller.rescue_action_in_public_without_airbrake)
             controller.rescue_action_in_public_without_airbrake(exception)
           end
-          render_exception_without_airbrake(env,exception)
+          render_exception_without_airbrake(env, exception)
         end
       end
     end
